d784c32396568c715859ba0c21ced93c517dee45
FruitWave <league@iMac-6.attlocal.net>
2018-08-09 19:08:03 -0700

    indentyd
    i have been shamed

--- test/HashMapCalculator.java
+++ test/HashMapCalculator.java
@@ -5,11 +5,11 @@ public class HashMapCalculator {
 	public int commonKeyValuePairs(HashMap<String, String> hashmap1, HashMap<String, String> hashmap2) {
 		int matches = 0;
 		for (String string1 : hashmap1.keySet()) {
-if (hashmap2.containsKey(string1)) {
-	if (hashmap2.get(string1).equals(hashmap1.get(string1))) {
-		matches++;
-	}
-}
+			if (hashmap2.containsKey(string1)) {
+				if (hashmap2.get(string1).equals(hashmap1.get(string1))) {
+					matches++;
+				}
+			}
 		}
 		return matches;
 	}
